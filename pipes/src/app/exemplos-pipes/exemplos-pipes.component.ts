1c77212faaa63882fd0baed27fb35bf0a7efa016
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-08-15 20:22:18 -0300

    Pipes (usando pipes, parâmetros e pipes aninhados)

---
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.scss']
+})
+export class ExemplosPipesComponent {
+
+  livro: any = {
+    titulo: 'Learning JavaScript',
+    rating: 4.545,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRp'
+  };
+
+}
