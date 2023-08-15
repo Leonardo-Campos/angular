8250f8281344a0047bd19c8540ce4ec25da7a15e
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-08-15 15:45:06 -0300

    Injetando um serviço em outro serviço

--- service/src/app/app.module.ts
+++ service/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
@@ -18,7 +18,7 @@ import { CursosModule } from './cursos/cursos.module';
     CriarCursoModule,
     CursosModule
   ],
-  // providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
