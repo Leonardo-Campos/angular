73d1a4556f387b00a4eb2080ded225fb6f6cc570
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-08-21 18:25:11 -0300

    Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
   styleUrls: ['./aluno-detalhe.component.scss']
 })
-export class AlunoDetalheComponent {
+export class AlunoDetalheComponent implements OnInit {
 
+  inscricao!: Subscription;
+  aluno: any;
+
+  constructor(
+    private router: ActivatedRoute,
+    private redirectRoute: Router,
+    private alunoService: AlunosService
+  ) {}
+
+  ngOnInit() {
+    this.inscricao = this.router.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunoService.getAluno(id);
+      }
+    );
+}
+
+ngOnDestroy() {
+  this.inscricao.unsubscribe();
+
+}
+
+editarContato() {
+  this.redirectRoute.navigate(['/alunos', this.aluno.id, 'editar']);
+}
 }
--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -11,6 +11,7 @@ import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,6 @@ import { AlunosRoutingModule } from './alunos.routing.module';
     FormsModule,
     MatFormFieldModule,
   ],
-  providers: []
+  providers: [AlunosService]
 })
 export class AlunosModule { }
