eb2669e98b07d0e3542315f5d7ff1c1535bb1f11
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-09-06 13:17:43 -0300

    Acesso ao FormControl no HTML e CSS de validação dos campos

--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { DataFormComponent } from './data-form.component';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule, MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,7 +17,16 @@ import { DataFormComponent } from './data-form.component';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    ReactiveFormsModule,
+    SharedModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatCheckboxModule,
+    MatPseudoCheckboxModule,
+    NgFor,
+    FormsModule,
+    MatIconModule
   ]
 })
 export class DataFormModule { }
