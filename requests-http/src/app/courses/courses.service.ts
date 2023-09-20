7ff7e45f64ffb9d95a9515af0310a8a5d6733caf
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-09-20 10:56:43 -0300

    Http: Dica: Variável de Ambiente

--- requests-http/src/app/courses/courses.service.ts
+++ requests-http/src/app/courses/courses.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from './course';
 import { tap } from 'rxjs/operators';
+import { environment } from './environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API ='http://localhost:3000/courses';
+  private readonly API =`${environment.API}courses`;
 
 constructor(private http: HttpClient) { }
 
